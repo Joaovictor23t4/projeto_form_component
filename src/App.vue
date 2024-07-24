40831102fbcece4738be7d22e66d040b2b4467fb
Joaovictor23t4 <joaovictor239090@gmail.com>
2024-07-24 10:59:49 -0300

    FEAT: Create project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import DisplayProfile from './components/DisplayProfile.vue';
+import EditProfile from './components/EditProfile.vue';
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped>
+</style>
